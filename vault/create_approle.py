de0b1848d8f3732116b2c6e5867b6fa058eeb8cf
Ori Hoch <ori@uumpa.com>
2022-09-19 10:50:19 +0300

    update vault create approle script

--- vault/create_approle.py
+++ vault/create_approle.py
@@ -4,26 +4,34 @@ import subprocess
 
 
 def main(role_name, policy_name):
-    p = subprocess.run(['vault', 'auth', 'enable', 'approle'], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    if p.returncode == 0:
-        print('AppRole auth method enabled')
-    else:
-        if p.stderr.decode().strip().endswith("path is already in use at approle/"):
-            print("Approle auth method was already enabled")
-        else:
-            raise Exception(p.stderr.decode())
+    res = json.loads(subprocess.check_output(["vault", "auth", "list", "-format=json"]))
+    if 'approle/' not in res:
+        subprocess.check_call(['vault', 'auth', 'enable', 'approle'])
+        res = json.loads(subprocess.check_output(["vault", "auth", "list", "-format=json"]))
+    approle_accessor = res['approle/']['accessor']
+    print(f'approle_accessor: {approle_accessor}')
     subprocess.check_call([
-        "vault", "write", f"auth/approle/role/{role_name}",
-        f'"token_policies="{policy_name}"',
-        'token_ttl=1h', 'token_max_ttl=4h'
+        "vault", "write", "identity/entity",
+        f'name=approle_{role_name}', f'policies={policy_name}',
+    ])
+    entity_id = json.loads(subprocess.check_output([
+        "vault", "read", "-format=json", f"identity/entity/name/approle_{role_name}"
+    ]))["data"]["id"]
+    print(f'entity_id: {entity_id}')
+    subprocess.check_call([
+        "vault", "write", f"auth/approle/role/{role_name}", 'token_ttl=1h', 'token_max_ttl=4h'
     ])
     role_id = json.loads(subprocess.check_output([
         "vault", "read", "-format=json", f"auth/approle/role/{role_name}/role-id"
     ]))["data"]["role_id"]
+    print(f'role_id: {role_id}')
+    subprocess.check_call([
+        "vault", "write", "identity/entity-alias",
+        f'name={role_id}', f'canonical_id={entity_id}', f'mount_accessor={approle_accessor}',
+    ])
     secret_id = json.loads(subprocess.check_output([
         "vault", "write", "-format=json", "-f", f"auth/approle/role/{role_name}/secret-id"
     ]))["data"]["secret_id"]
-    print(f"role_id: {role_id}")
     print(f"secret_id: {secret_id}")
 
 
